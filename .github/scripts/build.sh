b32ea40fe88d7b2920c3c6a77e94ffa59eb63aac
vijishankar <67778907+vijishankar@users.noreply.github.com>
2022-10-06 16:11:03 +0530

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,7 +3,7 @@ git fetch
 
 # Loop through all remote merged branches
 for branch in $(git branch -r | grep -v HEAD | grep -v joshua781467-patch-1 | grep -v master | sed /\*/d); do
-        if [ -z "$(git log -1 --since='Aug 10, 2022' -s ${branch})" ]; then
+        if [ -z "$(git log -1 --before='2 months ago' -s ${branch})" ]; then
                 echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
                 remote_branch=$(echo ${branch} | sed 's#origin/##' )
                 # To delete the branches uncomment the bellow git delete command
